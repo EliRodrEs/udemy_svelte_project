aeb8c6fdfb6f54d91d08dae968d8fc6c9518063e
EliRodrEs <60432604+EliRodrEs@users.noreply.github.com>
2022-12-14 21:21:13 +0100

    minor reformat

--- src/Meetups/EditMeetup.svelte
+++ src/Meetups/EditMeetup.svelte
@@ -3,45 +3,82 @@
   import Button from "../UI/Button.svelte"
   import Modal from "../UI/Modal.svelte"
 
-  import { createEventDispatcher } from 'svelte'
+  import { createEventDispatcher } from "svelte"
 
-  let title = ''
-  let subtitle = ''
-  let address = ''
-  let description = ''
-  let imageUrl = ''
-  let email = ''
+  let title = ""
+  let subtitle = ""
+  let address = ""
+  let description = ""
+  let imageUrl = ""
+  let email = ""
   const dispatch = createEventDispatcher()
 
   function submitForm() {
-    dispatch('save', {
+    dispatch("save", {
       title: title,
       subtitle: subtitle,
       address: address,
       description: description,
       imageUrl: imageUrl,
-      email: email
+      email: email,
     })
   }
 
-  function cancel(){
-    dispatch('cancel')
+  function cancel() {
+    dispatch("cancel")
   }
 </script>
 
 <Modal title="Edit Meetup Data" on:cancel>
   <form on:submit|preventDefault={submitForm}>
-    <TextInput controlType="text" id="title" label="Title" value={title} on:input={event => (title = event.target.value)} />
-    <TextInput controlType="text" id="subtitle" label="Subtitle" value={subtitle} on:input={event => (subtitle = event.target.value)} />
-    <TextInput controlType="text" id="address" label="Address" value={address} on:input={event => (address = event.target.value)} />
-    <TextInput controlType="text" id="imageUrl" label="Image URL" value={imageUrl} on:input={event => (imageUrl = event.target.value)} />
-    <TextInput controlType="email" id="email" label="Email" value={email} on:input={event => (email = event.target.value)} />
-    <TextInput id="description" controlType="textarea" rows="3" label="Description" value={description} on:input={event => (description = event.target.value)} />
-    </form>
-    <div slot="footer">
-      <Button type="button" mode="outline" on:click={cancel}>Cancel</Button>
-      <Button type="button" on:click={submitForm}>Save</Button>
-    </div>
+    <TextInput
+      controlType="text"
+      id="title"
+      label="Title"
+      value={title}
+      on:input={(event) => (title = event.target.value)}
+    />
+    <TextInput
+      controlType="text"
+      id="subtitle"
+      label="Subtitle"
+      value={subtitle}
+      on:input={(event) => (subtitle = event.target.value)}
+    />
+    <TextInput
+      controlType="text"
+      id="address"
+      label="Address"
+      value={address}
+      on:input={(event) => (address = event.target.value)}
+    />
+    <TextInput
+      controlType="text"
+      id="imageUrl"
+      label="Image URL"
+      value={imageUrl}
+      on:input={(event) => (imageUrl = event.target.value)}
+    />
+    <TextInput
+      controlType="email"
+      id="email"
+      label="Email"
+      value={email}
+      on:input={(event) => (email = event.target.value)}
+    />
+    <TextInput
+      id="description"
+      controlType="textarea"
+      rows="3"
+      label="Description"
+      value={description}
+      on:input={(event) => (description = event.target.value)}
+    />
+  </form>
+  <div slot="footer">
+    <Button type="button" mode="outline" on:click={cancel}>Cancel</Button>
+    <Button type="button" on:click={submitForm}>Save</Button>
+  </div>
 </Modal>
 
 <style>
